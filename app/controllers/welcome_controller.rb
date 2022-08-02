2273a4d3a1c6cc420dbef2b28e6df00c70baca65
kamkara <lnclass.kamkara@gmail.com>
2022-08-02 21:15:12 +0000

    app: add

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     redirect_to dashboard_path if user_signed_in?
-    @HomeArticles = Article.all.order('created_at desc') #where("status= ?","La lune").order("created_at desc").take(8)
+    #@HomeArticles = Article.all.order('created_at desc') #where("status= ?","La lune").order("created_at desc").take(8)
   end
 end
