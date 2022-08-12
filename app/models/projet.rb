0a11ee01de9494a226b4eb5cc8c4dbf7a89d358c
kamkara <lnclass.kamkara@gmail.com>
2022-08-12 23:38:18 +0000

    app: Add projet

--- app/models/projet.rb
+++ app/models/projet.rb
@@ -3,7 +3,7 @@ class Projet < ApplicationRecord
   has_rich_text :content
   #Projet.all.with_rich_text_content # Preload the body without attachments.
   #Projet.all.with_rich_text_content_and_embeds # Preload both body and attachments.
-
+  has_one_attached :heroImg
 
   STATUS= ["Encours", "Approbation", "Public", "La lune", "Archive"]
   validates :title, :content, :start_date, :amount, presence: true
