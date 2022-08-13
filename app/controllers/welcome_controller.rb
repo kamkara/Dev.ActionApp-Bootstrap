232b4a158f5f0e7286726d6e88dcecedcfa8bb2c
kamkara <lnclass.kamkara@gmail.com>
2022-08-13 18:10:56 +0000

    Add

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
     redirect_to dashboard_path if user_signed_in?
     @HomeArticles = Article.where("status= ?","La lune").order("created_at desc").take(8)
+    @HomeProjets = Projet.where("status= ?","La lune").order("created_at desc").take(4) #Projet a la lune
+    @HomeCampagnes = Campagne.order("created_at desc").take(4)#4 dernieres campagnes
   end
 end
