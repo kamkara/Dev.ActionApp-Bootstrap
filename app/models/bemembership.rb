35184cca5c0eb40d0abcb53f4d3183b6f4d0a366
kamkara <lnclass.kamkara@gmail.com>
2022-08-15 00:50:01 +0000

    Add update to home page fix

--- app/models/bemembership.rb
+++ app/models/bemembership.rb
@@ -2,8 +2,13 @@ class Bemembership < ApplicationRecord
     validates :first_name, :last_name, :email,
               :country, :contact, :gender, :terms, :citytown, presence: true
 
+    
     validates :first_name, :last_name,
               format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ },
               length: { maximum: 30,
               message: "%{value} verifier votre nom complet"}
+    validates :contact,
+              format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ },
+              length: { maximum: 14,
+              message: "%{value} verifier votre Numéro"}
 end
